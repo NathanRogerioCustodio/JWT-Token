487c3ea34c993a4500de278582a67c1720b66c7d
NathanRogerioCustodio <nathanrogerio95@hotmail.com>
2024-05-21 07:14:28 -0300

    last commit

---
+++ jwt-api/src/main/java/com/example/jwtapi/dto/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.example.jwtapi.dto;
+
+public record LoginRequestDTO (String email, String password){
+}
---
+++ jwt-api/src/main/java/com/example/jwtapi/dto/ResponseDTO.java
@@ -0,0 +1,4 @@
+package com.example.jwtapi.dto;
+
+public record ResponseDTO(String name, String token) {
+}
